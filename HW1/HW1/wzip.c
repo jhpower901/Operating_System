2ff650aa06ab7cf2c273a972370d08375ef09074
Ahnbujang <jhpower901@gmail.com>
2024-10-03 16:45:54 +0900

    wzip

---
+++ HW1/HW1/wzip.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* txt 파일 압축하여 stdout으로 출력하는 함수
+ * @param: txt 파일 위치
+ * @return: 1: 매개변수 X, file can not be opened
+ */
+int compress(char* fileLocation);
+
+int main(int argc, char** argv) {
+	if (argc < 2) {
+		printf("wzip: file1 [file2 ...]\n");
+		return 1;
+	}
+	else {
+		for (int i = 1; i < argc; i++)
+			compress(argv[i]);
+	}
+	return 0;
+}
+
+int compress(char* fileLocation) {
+	char current, previous;
+	int count = 0;
+
+	FILE* fp = NULL;
+	fp = fopen(fileLocation, "r");
+	if (fp == NULL) {
+		printf("wzip: cannot open file\n");
+		exit(1);
+	}
+
+	while (1) {
+		current = fgetc(fp);
+		if (current == EOF) {
+			if (count > 0) {
+				fwrite(&count, sizeof(int), 1, stdout);
+				fwrite(&previous, sizeof(char), 1, stdout);
+			}
+			break;
+		}
+
+		if (count > 0) {
+			if (current == previous)
+				count++;
+			else {
+				fwrite(&count, sizeof(int), 1, stdout);
+				fwrite(&previous, sizeof(char), 1, stdout);
+				count = 0;
+			}
+		}
+		else {
+			count++;
+			previous = current;
+		}
+	}
+	fclose(fp);
+}
